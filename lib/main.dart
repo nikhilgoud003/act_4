9426fc0a1882646522aa4c4e4c59ea7e22048106
koushik2002vas5 <koushiksai.vas5@gmail.com>
2025-02-12 19:14:27 -0500

    Added heart feature to application

--- lib/main.dart
+++ lib/main.dart
@@ -11,27 +11,69 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Valentine\'s Heartbeat',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      theme: ThemeData(primarySwatch: Colors.red),
       home: const HeartbeatScreen(),
     );
   }
 }
 
-class HeartbeatScreen extends StatelessWidget {
+class HeartbeatScreen extends StatefulWidget {
   const HeartbeatScreen({super.key});
+
+  @override
+  HeartbeatScreenState createState() => HeartbeatScreenState();
+}
+
+class HeartbeatScreenState extends State<HeartbeatScreen>
+    with TickerProviderStateMixin {
+  late AnimationController _heartbeatController;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _heartbeatController = AnimationController(
+      duration: const Duration(milliseconds: 1500),
+      vsync: this,
+    );
+
+    _scaleAnimation = Tween<double>(begin: 1.0, end: 1.2).animate(
+      CurvedAnimation(parent: _heartbeatController, curve: Curves.easeInOut),
+    );
+  }
+
+  void _startAnimation() {
+    _heartbeatController.repeat(reverse: true);
+  }
+
+  void _stopAnimation() {
+    _heartbeatController.stop();
+  }
+
+  @override
+  void dispose() {
+    _heartbeatController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.pink[50],
-      body: const Center(
-        child: Text(
-          'Celebrate Love with Us This Valentines Days',
-          style: TextStyle(fontSize: 24, color: Colors.pink),
+      body: Center(
+        child: ScaleTransition(
+          scale: _scaleAnimation,
+          child: Icon(
+            Icons.favorite,
+            size: 150,
+            color: Colors.red[700],
+          ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _startAnimation,
+        child: const Icon(Icons.favorite),
       ),
     );
   }
